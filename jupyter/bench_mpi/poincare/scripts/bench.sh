3203bd01b48f343239b37a02d2f76f889b0d06ca
Unknown <arnodurocher@gmail.com>
2018-06-08 11:10:07 +0200

    Added bench_mpi jupyter notebook

--- jupyter/bench_mpi/poincare/scripts/bench.sh
+++ jupyter/bench_mpi/poincare/scripts/bench.sh
@@ -1,8 +1,23 @@
 set -x
 
-llsubmit batch_1.sh
+strong_size=16
 
-for n in 1 2 4 8 16 32
+submit_batch(){
+    n_nodes=$1
+    n_proc=$2
+    size=$3
+    tree_h=$4
+    sed "s/<NB_NODES>/"${n_nodes}"/" batch.sh | sed "s/<NB_PROC>/"${n_proc}"/" | sed "s/<PB_SIZE>/"${size}"/" | sed "s/<TREE_H>/"${tree_h}"/" | llsubmit -
+} 
+
+for tree_h in 6 7 8
 do
-    sed "s/<NB_NODES>/"$n"/" batch.sh | llsubmit -
+    submit_batch 1 1 1 $tree_h
+    submit_batch 1 1 $strong_size $tree_h
+    for n in 1 2 4 8 16 32
+    do
+        let 'nproc = 2*n'
+        submit_batch $n $nproc $nproc $tree_h
+        submit_batch $n $nproc $strong_size $tree_h 
+    done
 done
